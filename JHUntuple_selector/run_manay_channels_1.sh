1b4e8f57f89166d2a6dc43efc05065616e10f807
Raymond <lfeng7.jhu@gmail.com>
2015-10-07 00:31:40 +0800

    add shell script to run all channels

---
+++ JHUntuple_selector/run_manay_channels_1.sh
@@ -0,0 +1,7 @@
+python selection.py --inputfiles pat_locations/T_s-channel_TuneZ2star_8TeV-powheg-tauola/\* --test no --maxfiles -1 --maxEvts -1 --type T_s --makeplots no
+python selection.py --inputfiles pat_locations/T_t-channel_TuneZ2star_8TeV-powheg-tauola/\* --test no --maxfiles -1 --maxEvts -1 --type T_t --makeplots no
+python selection.py --inputfiles pat_locations/T_tW-channel_TuneZ2star_8TeV-powheg-tauola/\* --test no --maxfiles -1 --maxEvts -1 --type T_tW --makeplots no
+python selection.py --inputfiles pat_locations/Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola/\* --test no --maxfiles -1 --maxEvts -1 --type Tbar_t --makeplots no
+python selection.py --inputfiles pat_locations/Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola/\* --test no --maxfiles -1 --maxEvts -1 --type Tbar_s --makeplots no
+python selection.py --inputfiles pat_locations/Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola/\* --test no --maxfiles -1 --maxEvts -1 --type Tbar_tW --makeplots no
+
