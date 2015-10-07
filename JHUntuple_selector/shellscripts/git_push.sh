61676e37b34f3a20bd2502d7b423179794d1be72
Raymond <lfeng7.jhu@gmail.com>
2015-10-07 22:05:18 +0800

    'add run_counter_nickntuples.sh'

--- JHUntuple_selector/shellscripts/git_push.sh
+++ JHUntuple_selector/shellscripts/git_push.sh
@@ -1,5 +1,4 @@
 read  -e -p "Enter the commit message: " commit_message
 read -e -p "Enter the branch name for push: " branch_name
-git add *.*
 git commit -m "'$commit_message'"
 git push origin "$branch_name"
---
+++ JHUntuple_selector/shellscripts/run_counter_nickntuples.sh
@@ -0,0 +1,14 @@
+# bkg
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_s/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_t/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_tW/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_s/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_t/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_tW/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/DY3JetsToLL_M/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/DY4JetsToLL_M/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/W4JetsToLNu_TuneZ2Star_8TeV/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/W3JetsToLNu_TuneZ2Star_8TeV/\*
+# signal
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/TT_CT10_TuneZ2star_8TeV/\*
+
