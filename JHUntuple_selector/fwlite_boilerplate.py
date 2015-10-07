053ddf42bc2175cb77734d9526e3d036a5ea9f3e
Raymond <lfeng7.jhu@gmail.com>
2015-10-07 04:11:15 -0500

    fix a bug in fwlite_boilerplate.py

--- JHUntuple_selector/fwlite_boilerplate.py
+++ JHUntuple_selector/fwlite_boilerplate.py
@@ -103,7 +103,7 @@ def saving(histlist,event_type='MC',index = ''):
         print 'Creating new dir '+savedir
     # Saving root files
     fout = ROOT.TFile(savedir+event_type+'_selection_output'+str(index)+'.root','recreate')
-    print 'saving output into file: '+fout
+    print 'saving output into file: '+savedir+event_type+'_selection_output'+str(index)+'.root'
     for ihist in histlist:
         ihist.Write()
     # file closure
