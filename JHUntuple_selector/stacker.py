f9ffbbbafdd532a26446d3a0416f47923a038046
Raymond <lfeng7.jhu@gmail.com>
2015-10-08 06:37:28 -0500

    debug stacker

--- JHUntuple_selector/stacker.py
+++ JHUntuple_selector/stacker.py
@@ -1,6 +1,7 @@
 # This small macro will read in all edm files in a directory and count the total number of events 
 
 from fwlite_boilerplate import *
+from ttbar_utitilty import *
 
 from optparse import OptionParser
 
@@ -40,36 +41,38 @@ data_lumi = 19.7*1000
 
 # Get input files
 prepend = './output_rootfiles/all_channels/'   # dir of output files to stack 
+  
+# initialization and declaration
+sample_types = []
+flist = []
+hlist_ = []
 
 # stucture of a list of files
 #    0,         1         2        3         4                 5
 # (filepath, nevts_gen, xsec_NLO, type, nevts_total_ntuple, nevts_used_ntuple)
-flist.extend(['T_s_selection_output_all.root',259176,3.79,'singletop',259176,259176] )
-flist.extend(['TT_CT10_selection_output_all.root',21560109,245.9,'ttbar',21560109,1150618])
+flist.append(['T_s_selection_output_all.root',259176,3.79,'singletop',259176,259176] )
+flist.append(['TT_CT10_selection_output_all.root',21560109,245.9,'ttbar',21560109,1150618])
 
 # list of histogram to make stack plots
 hlist = ['cutflow','jets_pt']
 
 # Booking stack histograms 
 for hist in hlist:
-    hlist_.extend(ROOT.THStack(hist,'undefined'))
+    hlist_.append(ROOT.THStack(hist,'undefined'))
 # put the name of histogram in the stack and the stack histograms in a list
 stacklist = zip(hlist,hlist_)
 
 # Make a legend
-leg = TLegend(0.3,0.6,0.6,0.8)
-
-# initialization and declaration
-sample_types = []
+leg = ROOT.TLegend(0.3,0.6,0.6,0.8)
 
 # Loop over files
-for ifile in filelist :
+for ifile in flist :
     # Getting files
     print 'processing file',ifile[0]
-    f_ = ROOT.TFile(ifile[0])
+    f_ = ROOT.TFile(prepend+ifile[0])
     # Find the color of the sample type
     sample_type = ifile[3]
-    icolor = sample_color[sample_type] # need to write a sub routine that return color given sample type
+    icolor = sample_color(sample_type) # need to write a sub routine that return color given sample type
 
     # Calculate weight for this channel
     fraction = ifile[5]*1.0/ifile[4]
@@ -103,7 +106,7 @@ for ifile in filelist :
         print 'weight is:',weight   
     
     # close temp file
-    f_.close()
+    f_.Close()
 
 ##### end loop over files ######
 
--- JHUntuple_selector/ttbar_utitilty.py
+++ JHUntuple_selector/ttbar_utitilty.py
@@ -7,12 +7,10 @@ def M3(jets):
     return jets_pt[0]+jets_pt[1]+jets_pt[2]
 
 def sample_color(sample_type):
-    type_color = [('singletop',ROOT.kMagenta),('ttbar',ROOT.kRed+1),('zjets',ROOT.kAzure-2)]
-    type_color.extend[('wjets',ROOT.kGreen-3)]    
-    color = [ icolor[1] for icolor in type_color if icolor[0] == sample_type]
-    if len(color)==1 :
-        return color[0]
+    if sample_type == 'singletop': return ROOT.kMagenta
+    elif sample_type == 'wjets'    : return ROOT.kGreen-3
+    elif sample_type == 'ttbar'    : return ROOT.kRed+1
+    elif sample_type == 'zjets'    : return ROOT.kAzure-2
     else :
         return 0
-        print 'the sample type is not in the color list!'
 
