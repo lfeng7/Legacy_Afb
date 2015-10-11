62521ff8c729bf6f858b7a1692b4e3ddbfcae129
Raymond <lfeng7.jhu@gmail.com>
2015-10-11 21:37:48 +0800

    'add items to run_counter_nickntuples.sh'

--- JHUntuple_selector/shellscripts/run_counter_nickntuples.sh
+++ JHUntuple_selector/shellscripts/run_counter_nickntuples.sh
@@ -1,14 +1,18 @@
 # bkg
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_s/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_t/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_tW/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_s/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_t/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_tW/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/DY3JetsToLL_M/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/DY4JetsToLL_M/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/W4JetsToLNu_TuneZ2Star_8TeV/\*
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/W3JetsToLNu_TuneZ2Star_8TeV/\*
-# signal
-python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/TT_CT10_TuneZ2star_8TeV/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_s/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_t/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/T_tW/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_s/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_t/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/Tbar_tW/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/DY3JetsToLL_M/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/DY4JetsToLL_M/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/W4JetsToLNu_TuneZ2Star_8TeV/\*
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/W3JetsToLNu_TuneZ2Star_8TeV/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/DY1JetsToLL_M/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/DY2JetsToLL_M/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/W1JetsToLNu_TuneZ2Star_8TeV/\*
+python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/W2JetsToLNu_TuneZ2Star_8TeV/\*
+# # signal
+# python count_evts.py --inputfiles ntuples/nick_jhu_diffmo/TT_CT10_TuneZ2star_8TeV/\*
 
