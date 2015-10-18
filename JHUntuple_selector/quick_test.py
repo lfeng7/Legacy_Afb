cdd1d431b10e72153cda9ba31c565b1e4c279a47
Raymond <lfeng7.jhu@gmail.com>
2015-10-18 19:35:15 +0800

    'testing vector(lorentzvector)'

--- JHUntuple_selector/quick_test.py
+++ JHUntuple_selector/quick_test.py
@@ -73,8 +73,8 @@ jet_flavor_label =("jhuAk5","AK5PartonFlavour")
 #jet_flavor_label =("pfShyftTupleJets","jetFlavor")
 
 # Book histograms
-h1 = ROOT.TH1D('met','met;GeV;events',100,0.,200.0)
-h2 = ROOT.TH1D('nels',';nels;events',5,0.5,5.5)
+h1 = ROOT.TH1D('jetspt','jetspt;pt GeV;events',50,0.,300.0)
+h2 = ROOT.TH1D('njets',';njets;events',5,3,8)
 
 h3 = ROOT.TH1D('csv_all_jets', type+' CSV of all jets;csv;events',100,0,1)
 h4 = ROOT.TH1D('csv_b_jets', type+' CSV of b jets;csv;events',100,-20,40)
@@ -88,7 +88,7 @@ fout = ROOT.TFile('test.root','recreate')
 # Add and book ttree
 testtree = ROOT.TTree('test','test')
 
-jetsp4 = ROOT.vector('TLorentzVector')
+jetsp4 = ROOT.vector('TLorentzVector')()
 testtree.Branch('jetsp4',jetsp4)
 # Counter initiation 
 n_evt = 0
@@ -105,6 +105,10 @@ for evt in events:
 
     evt.getByLabel(trig_label,trig_hndl)
     trig_ = trig_hndl.product()
+
+    # testing writing jets p4 into a vector of TLorentzVector  and then into the ttree
+
+    jetsp4.Clear()
     
     evt.getByLabel(jet_p4_label, jet_p4_hndl)
     jets_p4 = jet_p4_hndl.product() 
@@ -124,6 +128,21 @@ print 'break at event',n_evt
 fout.cd()
 testtree.Write()
 fout.Close()
+
+filein = ROOT.TFile('test.root')
+ttree_ = filein.Get('test')
+
+for i in range(ttree_.GetEntries()):
+    ttree_.GetEntry(i)
+    jetsp4 = ttree_.jetsp4
+    #Fill numjets and pt of jets
+    h2.Fill(jetsp4.size())
+    for ijet in jetsp4:
+        h1.Fill(ijet.Pt())
+
+plotting([h1,h2],'test','dump')
+filein.Close()
+
 
 # Plotting and saving 
 #histlist = [h3,h4,h5,h6]
