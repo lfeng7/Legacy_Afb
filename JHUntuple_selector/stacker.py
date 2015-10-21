abc3c3359e04f4654269a290686b9e8d10061238
Raymond <lfeng7.jhu@gmail.com>
2015-10-21 22:59:02 +0800

    'test stacker.py'

--- JHUntuple_selector/stacker.py
+++ JHUntuple_selector/stacker.py
@@ -59,7 +59,7 @@ dir_name = 'controlplots'
 Nbin = 2
 
 # Get input files
-prepend = './output_rootfiles/all_channels/'   # dir of output files to make histograms
+prepend = './output_rootfiles/all_selected/'   # dir of output files to make histograms
 hist_prepend = './selected_hists/test/' 
   
 # initialization and declaration
@@ -70,30 +70,30 @@ flist = []
 #    0,         1         2        3         4           
 # (filepath, nevts_gen, xsec_NLO, type, nevts_total_ntuple)
 # Single Top
-flist.append(['T_s_v2_selection_output_all','singletop'，259961,3.79,259176] )
-flist.append(['T_t_v2_selection_output_all.root','singletop',3758227,56.4,3748155] )
-flist.append(['T_tW_v2_selection_output_all.root','singletop',497658,11.1,495559])
-flist.append(['Tbar_s_v2_selection_output_all.root','singletop',139974, 1.76,139604])
-flist.append(['Tbar_t_v2_selection_output_all.root','singletop',1935072, 30.7,1930185])
-flist.append(['Tbar_tW_v2_selection_output_all.root','singletop',493460,11.1,491463])
-# Wjets
-flist.append(['W1JetsToLNu_v2.root','wjets',23141598,6662.8,23038253])
-flist.append(['W2JetsToLNu_v2.root','wjets',34044921,2159.2,33993463])
-flist.append(['W3Jets_v2_selection_output_all.root','wjets',15539503,640.4,15507852])
-flist.append(['W4Jets_v2_selection_output_all.root','wjets',13382803,246.0,13326400])
-# DYjets
-flist.append(['DY1JetsToLL_v2.root','zjets',24045248,660.6,23802736])
-flist.append(['DY2JetsToLL_v2.root','zjets',2352304,215.1,2345857])
-flist.append(['DY3Jets_v2_selection_output_all.root','zjets',11015445,65.79,10655325])
-flist.append(['DY4Jets_v2_selection_output_all.root','zjets',6402827,28.59,5843425])
+# flist.append(['T_s_v2_selection_output_all','singletop'，259961,3.79,259176] )
+# flist.append(['T_t_v2_selection_output_all.root','singletop',3758227,56.4,3748155] )
+# flist.append(['T_tW_v2_selection_output_all.root','singletop',497658,11.1,495559])
+# flist.append(['Tbar_s_v2_selection_output_all.root','singletop',139974, 1.76,139604])
+# flist.append(['Tbar_t_v2_selection_output_all.root','singletop',1935072, 30.7,1930185])
+# flist.append(['Tbar_tW_v2_selection_output_all.root','singletop',493460,11.1,491463])
+# # Wjets
+# flist.append(['W1JetsToLNu_v2.root','wjets',23141598,6662.8,23038253])
+# flist.append(['W2JetsToLNu_v2.root','wjets',34044921,2159.2,33993463])
+# flist.append(['W3Jets_v2_selection_output_all.root','wjets',15539503,640.4,15507852])
+# flist.append(['W4Jets_v2_selection_output_all.root','wjets',13382803,246.0,13326400])
+# # DYjets
+# flist.append(['DY1JetsToLL_v2.root','zjets',24045248,660.6,23802736])
+# flist.append(['DY2JetsToLL_v2.root','zjets',2352304,215.1,2345857])
+# flist.append(['DY3Jets_v2_selection_output_all.root','zjets',11015445,65.79,10655325])
+# flist.append(['DY4Jets_v2_selection_output_all.root','zjets',6402827,28.59,5843425])
 # signal
-flist.append(['TT_CT10_v2_selection_output_all.root','ttbar',21675970,245.9,21560109])
+flist.append(['TT_CT10_TuneZ2star_8TeV','ttbar',21675970,245.9,21560109])
 
 #### Set up data input files
 #    0,         1                   2             
 # (filepath,   type,   sample_integrated_lumi
-#datafile = ['SingleEl_Run2012A_v2_selection_output_all.root',888,19748,'data',11212832]
-datafile = ['SingleEl_Run2012ABCD_v2.root','data',19748]
+datafile = ['SingleEl_Run2012A_v1',888,19748,'data',11212832]
+#datafile = ['SingleEl_Run2012ABCD_v2.root','data',19748]
 
 def main():
     if options.makehists == 'yes':
@@ -181,8 +181,9 @@ def MakeComparisonPlots():
     print 'processing data file',data_input
     fdata = ROOT.TFile(data_input)
     # Calculate weight for data 
+    data_fraction = 20./205
     nevts_data = fdata.Get('cutflow').GetBinContent(1)
-    data_weight = data_lumi*1.0/datafile[2]
+    data_weight = data_lumi*1.0/(datafile[2]*data_fraction)
 
     data_hists = []
     # Get histograms from data
