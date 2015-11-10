63fdbf9e44be52fa1a6401aa3f2cbf0ee0dd0942
Raymond <lfeng7.jhu@gmail.com>
2015-11-10 15:23:11 +0800

    ''

---
+++ reco/test.py
@@ -0,0 +1,33 @@
+from Legacy_Afb.Tools.kinfit import *
+fin = ROOT.TFile('selected_files/v2_trigger_removed/all/TT_CT10_TuneZ2star_8TeV_selected.root')
+tmptree = fin.Get('selected')
+
+tmptree.GetEntry(10)
+
+jets_pt = tmptree.jets_pt
+jets_eta = tmptree.jets_eta
+jets_phi = tmptree.jets_phi
+jets_mass = tmptree.jets_mass
+jets_csv = tmptree.jets_csv 
+lep_pts = tmptree.lep_pt
+lep_etas = tmptree.lep_eta
+lep_phis = tmptree.lep_phis
+lep_mass = tmptree.lep_mass
+met_pt = tmptree.met_pt
+met_phi = tmptree.met_phi 
+
+# Set up inputs  
+lep_p4 = ROOT.TLorentzVector()
+lep_p4.SetPtEtaPhiM(lep_pt[0],lep_eta[0],lep_phi[0],lep_mass[0])
+jets_p4,jets_csv_list = [],[]    
+for i in range(jets_pt.size()):
+    tmp_p4 = ROOT.TLorentzVector()
+    tmp_p4.SetPtEtaPhiM(jets_pt[i],jets_eta[i],jets_phi[i],jets_phi[i],jets_mass[i])
+    jets_p4.append(tmp_p4)
+    jets_csv_list.append(jets_csv[i])
+metPt = met_pt[0]; metPhi = met_phi[0]
+# Do reco
+lep_type = 'el'
+mcordata = 'mc'
+
+reco_result = DoReco(jets_p4,jets_csv_list,lep_p4,metPt,metPhi,lep_type,mcordata)
