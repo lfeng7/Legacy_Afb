4e4165fa74dfd545007fa9ad21d523936a5a043f
Raymond <lfeng7.jhu@gmail.com>
2015-11-13 02:36:10 +0800

    'update condor_fakelep to use latest ntuples from nick'

--- JHUntuple_selector/condorjobs_fakelep/W1JetsToLNu_TuneZ2Star_8TeV/grid_sub.csh
+++ JHUntuple_selector/condorjobs_fakelep/W1JetsToLNu_TuneZ2Star_8TeV/grid_sub.csh
@@ -1,5 +1,5 @@
 #! /bin/sh
-tar czvf tarball.tgz ../../selection.py ../../fwlite_boilerplate.py  ../../utility inputfiles
+tar czvf tarball.tgz ../../selection.py  inputfiles
 
 voms-proxy-init --voms cms
 
--- JHUntuple_selector/condorjobs_fakelep/update_all.sh
+++ JHUntuple_selector/condorjobs_fakelep/update_all.sh
@@ -1,22 +1,44 @@
+cp -r template/inputfiles DY1JetsToLL_M
 cp  template/grid_sub.csh DY1JetsToLL_M
+cp -r template/inputfiles DY2JetsToLL_M
 cp  template/grid_sub.csh DY2JetsToLL_M
+cp -r template/inputfiles DY3JetsToLL_M
 cp  template/grid_sub.csh DY3JetsToLL_M
+cp -r template/inputfiles DY4JetsToLL_M
 cp  template/grid_sub.csh DY4JetsToLL_M
+cp -r template/inputfiles QCD_Pt-15to3000
 cp  template/grid_sub.csh QCD_Pt-15to3000
+cp -r template/inputfiles SingleEl_Run2012A
 cp  template/grid_sub.csh SingleEl_Run2012A
+cp -r template/inputfiles SingleEl_Run2012B
 cp  template/grid_sub.csh SingleEl_Run2012B
+cp -r template/inputfiles SingleEl_Run2012C_part1
 cp  template/grid_sub.csh SingleEl_Run2012C_part1
+cp -r template/inputfiles SingleEl_Run2012C_part2
 cp  template/grid_sub.csh SingleEl_Run2012C_part2
+cp -r template/inputfiles SingleEl_Run2012D
 cp  template/grid_sub.csh SingleEl_Run2012D
+cp -r template/inputfiles T_s
 cp  template/grid_sub.csh T_s
+cp -r template/inputfiles T_t
 cp  template/grid_sub.csh T_t
+cp -r template/inputfiles T_tW
 cp  template/grid_sub.csh T_tW
+cp -r template/inputfiles Tbar_s
 cp  template/grid_sub.csh Tbar_s
+cp -r template/inputfiles Tbar_t
 cp  template/grid_sub.csh Tbar_t
+cp -r template/inputfiles Tbar_tW
 cp  template/grid_sub.csh Tbar_tW
+cp -r template/inputfiles template
 cp  template/grid_sub.csh template
+cp -r template/inputfiles TT_CT10_TuneZ2star_8TeV
 cp  template/grid_sub.csh TT_CT10_TuneZ2star_8TeV
+cp -r template/inputfiles W1JetsToLNu_TuneZ2Star_8TeV
 cp  template/grid_sub.csh W1JetsToLNu_TuneZ2Star_8TeV
+cp -r template/inputfiles W2JetsToLNu_TuneZ2Star_8TeV
 cp  template/grid_sub.csh W2JetsToLNu_TuneZ2Star_8TeV
+cp -r template/inputfiles W3JetsToLNu_TuneZ2Star_8TeV
 cp  template/grid_sub.csh W3JetsToLNu_TuneZ2Star_8TeV
+cp -r template/inputfiles W4JetsToLNu_TuneZ2Star_8TeV
 cp  template/grid_sub.csh W4JetsToLNu_TuneZ2Star_8TeV
