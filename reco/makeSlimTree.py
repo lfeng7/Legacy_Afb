57ca86c76998b3de0b5ce53c47ec5c6038559d30
Raymond <lfeng7.jhu@gmail.com>
2015-12-01 16:48:28 +0800

    'polish makeSlimTree.py to be more versatile'

--- reco/makeSlimTree.py
+++ reco/makeSlimTree.py
@@ -1,10 +1,34 @@
 import ROOT
-fin = ROOT.TFile("TT_CT10_TuneZ2star_8TeV_reco.root")
+import glob
+from optparse import OptionParser
+# Job steering
+
+# Input inputFiles to use. This is in "glob" format, so you can use wildcards.
+# If you get a "cannot find file" type of error, be sure to use "\*" instead
+# of "*" to make sure you don't confuse the shell. 
+
+parser = OptionParser()
+
+parser.add_option('--inputfiles', metavar='F', type='string', action='store',
+                  default = "",
+                  dest='inputFiles',
+                  help='Input files')
+(options, args) = parser.parse_args()
+
+argv = []
+
+postfix = '_slim.root'
+
+fin = ROOT.TFile(options.inputFiles)
 oldtree =fin.Get('selected')
-fout = ROOT.TFile('TT_CT10_TuneZ2star_8TeV_reco_slim.root','recreate')
+sample_name = options.inputFiles.split('/')
+sample_name = sample_name[len(sample_name)-1].split('.root')[0]
+
+fout = ROOT.TFile(sample_name+postfix,'recreate')
 oldtree.SetBranchStatus('*',0)
 oldtree.SetBranchStatus('*chi2*',1)
 oldtree.SetBranchStatus('reco*',1)
+oldtree.SetBranchStatus('jets*',1)
 newtree = oldtree.CloneTree()
 fout.Write()
 fout.Close()
