cd8842acd778885b93463b1815ab17d6ee29c0bd
Raymond <lfeng7.jhu@gmail.com>
2015-12-16 18:21:38 +0800

    'update stacker for selection control plots'

--- JHUntuple_selector/stacker.py
+++ JHUntuple_selector/stacker.py
@@ -74,7 +74,7 @@ parser.add_option('--fakelep', metavar='F', type='string', action='store',
                   help='If run on selected events with fake lepton.')
 
 parser.add_option('--lepisocut', metavar='F', type='int', action='store',
-                  default = 0.15,
+                  default = 0.2,
                   dest='lepisocut',
                   help='Lower bound for fake electron isolation.')
 
@@ -118,25 +118,25 @@ flist = []
 
 #    0,                 1         2          3         4                   5
 # (MC_sample_name, sample_type, Nevts_gen, x-sec, nevts_total_ntuple, btag_type)
-# # Single Top
-# flist.append(['T_s','singletop',259961,3.79,259176,'singletop'] )
-# flist.append(['T_t','singletop',3758227,56.4,3748155,'singletop'] )
-# flist.append(['T_tW','singletop',497658,11.1,495559,'singletop'])
-# flist.append(['Tbar_s','singletop',139974, 1.76,139604,'singletopbar'])
-# flist.append(['Tbar_t','singletop',1935072, 30.7,1930185,'singletopbar'])
-# flist.append(['Tbar_tW','singletop',493460,11.1,491463,'singletopbar'])
-# # Wjets
-# flist.append(['W1JetsToLNu_TuneZ2Star_8TeV','wjets',23141598,6662.8,23038253,'wjets'])
-# flist.append(['W2JetsToLNu_TuneZ2Star_8TeV','wjets',34044921,2159.2,33993463,'wjets'])
-# flist.append(['W3JetsToLNu_TuneZ2Star_8TeV','wjets',15539503,640.4,15507852,'wjets'])
-# flist.append(['W4JetsToLNu_TuneZ2Star_8TeV','wjets',13382803,246.0,13326400,'wjets'])
-# # DYjets
-# flist.append(['DY1JetsToLL_M','zjets',24045248,660.6,23802736,'zjets'])
-# flist.append(['DY2JetsToLL_M','zjets',2352304,215.1,2345857,'zjets'])
-# flist.append(['DY3JetsToLL_M','zjets',11015445,65.79,10655325,'zjets'])
-# flist.append(['DY4JetsToLL_M','zjets',6402827,28.59,5843425,'zjets'])
+# Single Top
+flist.append(['T_s','singletop',259961,3.79,259176,'singletop'] )
+flist.append(['T_t','singletop',3758227,56.4,3748155,'singletop'] )
+flist.append(['T_tW','singletop',497658,11.1,495559,'singletop'])
+flist.append(['Tbar_s','singletop',139974, 1.76,139604,'singletopbar'])
+flist.append(['Tbar_t','singletop',1935072, 30.7,1930185,'singletopbar'])
+flist.append(['Tbar_tW','singletop',493460,11.1,491463,'singletopbar'])
+# Wjets
+flist.append(['W1JetsToLNu_TuneZ2Star_8TeV','wjets',23141598,6662.8,23038253,'wjets'])
+flist.append(['W2JetsToLNu_TuneZ2Star_8TeV','wjets',34044921,2159.2,33993463,'wjets'])
+flist.append(['W3JetsToLNu_TuneZ2Star_8TeV','wjets',15539503,640.4,15507852,'wjets'])
+flist.append(['W4JetsToLNu_TuneZ2Star_8TeV','wjets',13382803,246.0,13326400,'wjets'])
+# DYjets
+flist.append(['DY1JetsToLL_M','zjets',24045248,660.6,23802736,'zjets'])
+flist.append(['DY2JetsToLL_M','zjets',2352304,215.1,2345857,'zjets'])
+flist.append(['DY3JetsToLL_M','zjets',11015445,65.79,10655325,'zjets'])
+flist.append(['DY4JetsToLL_M','zjets',6402827,28.59,5843425,'zjets'])
 # QCD
-flist.append(['QCD_Pt-15to3000','qcd',9991674,6662.6,9940092,'qcd'])
+# flist.append(['QCD_Pt-15to3000','qcd',9991674,6662.6,9940092,'qcd'])
 # signal
 flist.append(['TT_CT10_TuneZ2star_8TeV','ttbar',21675970,245.9,21560109,'ttbar'])
 
@@ -212,7 +212,7 @@ def MakeHistograms():
                 #######################################################
                 # trigger
                 if options.applytrigger == 'yes':
-                    if tmptree.trigger[0] == 0 : continue
+                    if not tmptree.trigger[0]: continue
                 # jets
                 bjets = []
                 for ijet in tmptree.jets_csv:
@@ -298,7 +298,7 @@ def MakeHistograms():
                     #######################################################
                     # trigger
                     if options.applytrigger == 'yes':
-                        if tmptree.trigger[0] == 0 : continue
+                        if not tmptree.trigger[0] : continue
                     # jets
                     bjets = []
                     for ijet in tmptree.jets_csv:
