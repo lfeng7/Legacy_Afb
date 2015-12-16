e2521d626ee685c364129919c0aa18d5bec8ef65
Raymond <lfeng7.jhu@gmail.com>
2015-12-16 04:23:15 -0600

    'update some condor script in selection'

---
+++ JHUntuple_selector/condorjobs_fakelep/update_inputfiles.sh
@@ -0,0 +1,42 @@
+rm -r DY1JetsToLL_M/inputfiles
+cp -r template/inputfiles DY1JetsToLL_M
+rm -r DY2JetsToLL_M/inputfiles
+cp -r template/inputfiles DY2JetsToLL_M
+rm -r DY3JetsToLL_M/inputfiles
+cp -r template/inputfiles DY3JetsToLL_M
+rm -r DY4JetsToLL_M/inputfiles
+cp -r template/inputfiles DY4JetsToLL_M
+rm -r QCD_Pt-15to3000/inputfiles
+cp -r template/inputfiles QCD_Pt-15to3000
+rm -r SingleEl_Run2012A/inputfiles
+cp -r template/inputfiles SingleEl_Run2012A
+rm -r SingleEl_Run2012B/inputfiles
+cp -r template/inputfiles SingleEl_Run2012B
+rm -r SingleEl_Run2012C_part1/inputfiles
+cp -r template/inputfiles SingleEl_Run2012C_part1
+rm -r SingleEl_Run2012C_part2/inputfiles
+cp -r template/inputfiles SingleEl_Run2012C_part2
+rm -r SingleEl_Run2012D/inputfiles
+cp -r template/inputfiles SingleEl_Run2012D
+rm -r TT_CT10_TuneZ2star_8TeV/inputfiles
+cp -r template/inputfiles TT_CT10_TuneZ2star_8TeV
+rm -r T_s/inputfiles
+cp -r template/inputfiles T_s
+rm -r T_t/inputfiles
+cp -r template/inputfiles T_t
+rm -r T_tW/inputfiles
+cp -r template/inputfiles T_tW
+rm -r Tbar_s/inputfiles
+cp -r template/inputfiles Tbar_s
+rm -r Tbar_t/inputfiles
+cp -r template/inputfiles Tbar_t
+rm -r Tbar_tW/inputfiles
+cp -r template/inputfiles Tbar_tW
+rm -r W1JetsToLNu_TuneZ2Star_8TeV/inputfiles
+cp -r template/inputfiles W1JetsToLNu_TuneZ2Star_8TeV
+rm -r W2JetsToLNu_TuneZ2Star_8TeV/inputfiles
+cp -r template/inputfiles W2JetsToLNu_TuneZ2Star_8TeV
+rm -r W3JetsToLNu_TuneZ2Star_8TeV/inputfiles
+cp -r template/inputfiles W3JetsToLNu_TuneZ2Star_8TeV
+rm -r W4JetsToLNu_TuneZ2Star_8TeV/inputfiles
+cp -r template/inputfiles W4JetsToLNu_TuneZ2Star_8TeV
