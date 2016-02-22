6643641c9c220e3fdf46c7bd95803da7792dcaaa
Raymond <lfeng7.jhu@gmail.com>
2016-02-21 20:35:54 -0600

    'finish add_branch.py'

--- add_branch.py
+++ add_branch.py
@@ -12,21 +12,21 @@ parser = OptionParser()
 
 parser.add_option('--file1', metavar='F', type='string', action='store',
                   default='',
-                  dest='the file to be cloned',
-                  help='')
+                  dest='file1',
+                  help='the file to be cloned')
 parser.add_option('--file2', metavar='F', type='string', action='store',
                   default='',
-                  dest='the file with a new branch to add',
-                  help='')
+                  dest='file2',
+                  help='the file with a new branch to add')
 
 (options, args) = parser.parse_args()
 
-f1 = options.file1
-f2 = options.file2
+file1 = options.file1
+file2 = options.file2
 branch = 'final_chi2'
 
-file1 = ROOT.TFile(f1)
-file2 = ROOT.TFile(f2)
+f1 = ROOT.TFile(file1)
+f2 = ROOT.TFile(file2)
 
 # Find the name of the ttree
 keys = f1.GetListOfKeys()
@@ -35,8 +35,10 @@ for ikey in keys:
 print 'Getting ttree',treename
 
 # create a new root file
-fname = f1.split('.root')[0]
+fname = file1.split('/')[-1]
+fname = fname.split('.root')[0]
 fname += '_new.root'
+print 'Save new tree into',fname
 f_new = ROOT.TFile(fname,'recreate')
 
 # clone tree 1
@@ -48,7 +50,8 @@ newtree = tree1.CloneTree(0)
 
 # add new branch from tree2 to newtree
 br = array('f',[0.])
-newtree.Branch(branch+'_new',br,'br/F')
+br_name = branch+'_new'
+newtree.Branch(br_name,br,br_name+'/F')
 
 # Loop over events
 nevts = tree1.GetEntries()
@@ -56,6 +59,7 @@ if nevts != tree2.GetEntries():
     print 'num entries of two files are not the same. Cannot add branch to file1.'
     sys.exit()
 for iev in range(nevts):
+    tree1.GetEntry(iev)
     tree2.GetEntry(iev)
     br[0] = tree2.final_chi2[0]
     newtree.Fill()
