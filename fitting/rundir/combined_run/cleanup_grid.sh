991f948360034314561289adb995f0440498a351
Raymond <lfeng7.jhu@gmail.com>
2016-03-03 14:00:49 -0600

    'change logL hist range during fitting'

--- fitting/rundir/combined_run/cleanup_grid.sh
+++ fitting/rundir/combined_run/cleanup_grid.sh
@@ -6,9 +6,10 @@ mv f_*_distribution.pdf final_fit_stuff
 mv all_data_histos*.* final_fit_stuff
 mv all_data_angles.* final_fit_stuff
 #mv aggregated_distributions.root final_fit_stuff
-#mv fit_results.txt final_fit_stuff
-#mv fit_comparison.pdf final_fit_stuff
+mv fit_results.txt final_fit_stuff
+mv fit_comparison.pdf final_fit_stuff
 mv sideband.pdf final_fit_stuff
-mv nominal_1M_output/final_fit_stuff/fit_results.txt fit_results/
+mv data_run_combined_output/final_fit_stuff/fit_results.txt fit_results/
 mv fit_results.txt fit_results/
 mv fit_comparison.png final_stack.root  fit_results/
+mv fit_results data_run_combined_output
