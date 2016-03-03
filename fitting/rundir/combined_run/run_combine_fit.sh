291c37e25536222293ce4b63a0bbdd623c3e903f
Raymond <lfeng7.jhu@gmail.com>
2016-03-03 16:38:33 -0600

    'modify logL range in fitting'

--- fitting/rundir/combined_run/run_combine_fit.sh
+++ fitting/rundir/combined_run/run_combine_fit.sh
@@ -1,2 +1,3 @@
-root -l -b -q '../../main_data.C(0,0,0,"data_fit_combined","MC_input_with_bkg.txt","data_input.txt")'
+#! /bin/bash
+root -l -b -q '../../main_data.C(0,0,0,"data_run_combined","MC_input_with_bkg.txt","data_input.txt")'
 source cleanup_grid.sh
