6afaf25cfe4a75d08caf04fedaea4b4e26f0ab82
Raymond <lfeng7.jhu@gmail.com>
2016-03-03 10:44:20 -0600

    'modify run_combine_fit.sh'

--- fitting/rundir/combined_run/run_combine_fit.sh
+++ fitting/rundir/combined_run/run_combine_fit.sh
@@ -1,2 +1,2 @@
-root -l -b -q '../../main_data.C(0,0,0,"nominal_1M","MC_input_with_bkg.txt","data_input.txt")'
+root -l -b -q '../../main_data.C(0,0,0,"data_fit_combined","MC_input_with_bkg.txt","data_input.txt")'
 source cleanup_grid.sh
